9c4120005fba43ab6cd11f6397f5afb9ed75c8e5
Satyam Tiwari <satyamtiwari1598@gmail.com>
2025-05-11 23:37:52 +0530

    minor bug fix

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ def create_contact():
     return jsonify({"message": "User Created!"}), 201
 
 
-@app.route("/update_contact/<int: user_id>", methods=["PATCH"])
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
 def update_contact(user_id):
     contact = Contact.query.get(user_id)
 
@@ -53,7 +53,7 @@ def update_contact(user_id):
     return jsonify({"message": "User Updated"}), 200
 
 
-@app.route("/delete_contact/<int: user_id>", methods=["DELETE"])
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
 def delete_contact(user_id):
     contact = Contact.query.get(user_id)
 
